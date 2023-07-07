006e4fad5c7866440c248008902bf70d47ba1aa1
akash yadav <64635004+akash4142@users.noreply.github.com>
2023-07-07 10:50:38 -0400

    Update store-service.js

--- store-service.js
+++ store-service.js
@@ -89,17 +89,24 @@ module.exports.addItem = function (itemData) {
 };
 
 
-module.exports.getItemsByCategory = function (category) {
-  return new Promise((resolve, reject) => {
-    const filteredItems = items.filter((item) => item.category === category);
-
-    if (filteredItems.length === 0) {
-      reject('No results returned');
-    }
-
-    resolve(filteredItems);
-  });
-};
+module.exports.getPublishedItemsByCategory = (category)=>{
+  return new Promise((resolve,reject)=>{
+      let pubItems = [];
+      for (let i=0;i<items.length;i++)
+      {
+          if(items[i].published==true && items[i].category == category)
+          {
+              pubItems.push(items[i]);
+          }
+      }
+      if(pubItems.length==0)
+      {
+          reject("no matching items");
+      }else{
+          resolve(pubItems);
+      }
+  })
+}
 
 module.exports.getPublishedItemsByCategory = function(category) {
   return new Promise((resolve, reject) => {
